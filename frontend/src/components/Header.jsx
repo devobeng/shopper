d5903bd2df31f6173f22d360ca2542e61e8f3b00
Bismark Obeng <bismarkstoney@yahoo.com>
2024-04-23 12:01:26 +0000

    added header and navbar components

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import logo from '../assets/logo.png';
+const Header = () => {
+	return (
+		<header>
+			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+				<Container>
+					<Navbar.Brand href='/'>
+						<img src={logo} alt='logo' />
+						Shopper
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls='basic-navbar-nav' />
+					<Navbar.Collapse id='basic-navbar-nav'>
+						<Nav className='ms-auto'>
+							<Nav.Link href='/card'>
+								<FaShoppingCart /> Cart
+							</Nav.Link>
+							<Nav.Link href='/login'>
+								<FaUser /> Login
+							</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</header>
+	);
+};
+
+export default Header;
